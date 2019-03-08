310dd459f2f599a78080325c46b93e4c467850f7
leslie <335074910@qq.com>
2019-03-08 16:07:33 +0800

    git web change 2

--- SourceTree/SourceTree/AppDelegate.swift
+++ SourceTree/SourceTree/AppDelegate.swift
@@ -17,7 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        // git web change
+        // git web change1
+        // git web change2
         
         
         return true
